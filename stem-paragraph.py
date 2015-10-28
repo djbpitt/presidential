f024ceb4ea5c2925d0f82705ae56a2a0e74e853b
David J Birnbaum <djbpitt@gmail.com>
2015-10-28 14:33:54 -0400

    Process one paragraph at a time

---
+++ stem-paragraph.py
@@ -0,0 +1,48 @@
+# stem-paragraph.py
+#
+# Synopsis: Lowercases and stems the first <body> descendant of
+#           English-language XML input file
+#
+# Syntax: python stem-paragraph.py filename.xml
+# Input:  filename.xml, specified on command line
+# Output: filename-x.txt (where x is a positive integer)
+# Requires: Python 3
+# djbpitt@gmail.com
+# http://www.obdurodon.org
+
+import sys, nltk
+from lxml import etree
+from nltk import word_tokenize
+
+counter = 0
+
+# Read input filename from command line
+filename = sys.argv[1]
+
+# Save filename without '.xml' extension to create output filename
+basename = filename.split('.')[0]
+
+# Open input file and read contents
+with open(sys.argv[1], 'rb') as f:
+    contents = f.read()
+
+# Extract <p> elements with XPath
+xml = etree.XML(contents)
+paragraphs = xml.xpath('//p')
+
+#Process each paragraph
+for paragraph in paragraphs:
+    # Prepare output file
+    counter = counter++
+    output = open(basename + '_p-' + counter + '.txt','w')
+
+    # Tokenize, lowercase, and stem
+    tokens = word_tokenize(paragraph.xpath('string()'))
+    words = [w.lower() for w in tokens]
+    wnl=nltk.WordNetLemmatizer()
+    lemmata = [wnl.lemmatize(w) for w in words]
+
+    # Write output file
+    for lemma in lemmata:
+        print(lemma, file=output)
+    output.close()
